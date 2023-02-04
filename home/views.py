e5d1b079976a71383d0a23c2fbcaf78f66a461f9
Aleksander Litvin <alexlik73@gmail.com>
2023-02-04 13:20:33 +0300

    add queryset

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home/index.html', {})
+    posts = Post.objects \
+        .filter(published_date__lte=timezone.now()) \
+        .order_by('published_date')
+    return render(
+        request, 
+        'home/test_index.html', 
+        {'posts':posts}
+        )
